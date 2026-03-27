99f97dfcdd0c659422bf52ad1a8f7369855df1d3
SolidSn4ke <artsuper472@gmail.com>
2026-03-27 10:06:58 +0300

    add more points for integ tests

--- src/test/java/itmo/st/lab2/integrational/IntegTestLvl3.java
+++ src/test/java/itmo/st/lab2/integrational/IntegTestLvl3.java
@@ -47,7 +47,7 @@ public class IntegTestLvl3 {
 
     LogBase log;
 
-    Double eps = 10e-4;
+    Double eps = 10e-2;
 
     Map<Double, Double> testArgs = Map.ofEntries(
             Map.entry(-5.68215, 4.39988),
@@ -58,7 +58,21 @@ public class IntegTestLvl3 {
             Map.entry(-2.27421, -0.2375),
             Map.entry(-1.5708, 0.0),
             Map.entry(0.34597, 0.0),
-            Map.entry(3.53668, 0.0));
+            Map.entry(3.53668, 0.0),
+            Map.entry(-5.8, 6.008352855446355),
+            Map.entry(-5.6, 5.006282373242017),
+            Map.entry(-5.0, 0.6043034915513443),
+            Map.entry(-4.5, -0.43623927648114386),
+            Map.entry(-3.9, -0.972342279807566),
+            Map.entry(-3.8, 202.06416382292076),
+            Map.entry(-2.4, 17.48043125773551),
+            Map.entry(-2.3, -0.19597159337319517),
+            Map.entry(-2.0, -4.565141898308976e-2),
+            Map.entry(-1.0, 398.37188654075703),
+            Map.entry(0.3, -0.8142632471987838),
+            Map.entry(0.5, 2.896984729815482),
+            Map.entry(2.0, -4.021795927368153),
+            Map.entry(4.0, 0.6058388607809142));
 
     @BeforeEach
     void injectMocks() {
